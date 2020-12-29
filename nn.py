ec277301f20b61070522daa69ce6a21430cce005
rising1 <phfrost@btconnect.com>
2020-12-29 20:59:29 +0000

    next

--- nn.py
+++ nn.py
@@ -4,88 +4,117 @@ class NN:
 
     def __init__(self):
 
-        self.weights1 = np.random.rand(3, 1)
+        self.weights1 = [[0.5],[0.5],[0.5]]  #  np.random.rand(3, 1)
         print("\n self.weights1= ", self.weights1)
-        self.biases1 = np.random.rand(1,)
+        self.biases1 = [[0.5]]  #   np.random.rand(1,)
         print("\n self.biases1= ",self.biases1,"\n")
 
 
     def feedforward(self, batch):
-
         # multiply by weights and add bias
-        self.batch = np.reshape(batch,[1,3])
-        self.z1 = self.batch.dot(self.weights1) + self.biases1
-        self.a1 = np.maximum(0,self.z1) # this is the activation function # **** This is working as expected
+        self.batch = [batch] #    np.reshape(batch,[1,3])
+        # self.z1 = self.dotOperation("multiply_elements",self.batch, self.weights1) + self.biases1
+        self.z1 = np.dot(self.batch,self.weights1) + self.biases1
+        # self.z1 = self.batch.dot(self.weights1) + self.biases1
+        # print("z1= ",self.z1)
+        self.a1 = np.maximum(0,self.z1) # this is the activation function
         return self.a1
-
 
     def calc_error(self,target):
         self.error = (self.a1 - target)
         self.meanSquaredError = np.mean(self.error**2)
 
-
-
-    def back_prop(self,batch):
+    def back_prop(self,batch,learning_rate):
         # back propagation chain rule calculus
-        learning_rate = 0.01
         self.dz1 = np.zeros_like(self.z1)
+        # self.dz1 = self.z1
+        # if(self.z1[0][0] > 0):
+        #    self.dz1[0][0] = 1
+        #else:
+        #    self.dz1[0][0] = 0
         self.dz1[self.z1 <= 0] = 0
         self.dz1[self.z1 > 0] = 1
         self.batchT = np.reshape(batch,[3,1])
-        self.gradientWeights = 2/self.batchT.shape[1] * np.dot( self.batchT,self.error * self.dz1)
+        # self.gradientWeights = 2/self.batchT.shape[1] * self.dotOperation("add_elements", self.batchT,self.error * self.dz1)
+        self.gradientWeights = 2 / self.batchT.shape[1] * np.dot(self.batchT, self.error * self.dz1)
         self.gradientBias = np.sum(self.error, axis=0, keepdims=True)
         self.weights1Adj  = - learning_rate * self.gradientWeights
         self.biases1Adj = - learning_rate * self.gradientBias
         self.weights1 = self.weights1 + self.weights1Adj
         self.biases1 = self.biases1 + self.biases1Adj
 
+    # Utility function for matrix operations
+    def dotOperation(self,operation,matrix1,matrix2):
+        a = len(matrix1)
+        b = len(matrix1[0])
+        c = len(matrix2)
+        d = len(matrix2[0])
+
+        #print("\ncode for dot matrix multiplication")
+        #print("matrix1 shape= (",len(matrix1),len(matrix1[0]),")")
+        #print("matrix2 shape= (",len(matrix2),len(matrix2[0]),")")
+        if(a != c or b != d):
+            print("\n","error - matrices should be same shape")
+            print(" matrix 1= (",a,b,") matrix 2= (",c,d,")")
+        result = [[None for col in range(a)] for row in range(b)]
+        print(result)
+        print("a= ",a)
+        if(c>0):
+            for i in range(c):
+                for j in range(d):
+                    print("j=",j," i=",i)
+                    print("multiplying ", matrix1[j][i], matrix2[i][j])
+                if operation == "multiply_elements":
+                    result[i][j] = matrix1[j][i] * matrix2[i][j]
+                if operation == "add_elements":
+                    result[i][j] = matrix1[j][i] + matrix2[i][j]
+        sum = 0.0
+        for res in result:
+            sum = sum + res[0]
+        print("sum= ", [[sum]])
+        return [[sum]]
 
 
 
 
 if __name__ == '__main__':
 
-    input_set = np.array([[0, 1, 0],
-                          [0, 0, 1],
-                          [1, 0, 0],
-                          [1, 1, 0],
-                          [1, 1, 1],
-                          [0, 0, 0]])  # Dependent variable
+    input_set = [[0, 1, 0],
+                 [0, 0, 1],
+                 [1, 0, 0],
+                 [1, 1, 0],
+                 [1, 1, 1],
+                 [0, 0, 0]]  # Dependent variable
 
-    labels = np.array([[1],
-                       [0],
-                       [0],
-                       [1],
-                       [1],
-                       [0]])
+    labels = [[1],
+              [0],
+              [0],
+              [1],
+              [1],
+              [0]]
 
     mynn = NN()
 
-    for i in range(100):
+    # Parameters learning rate and number of iterations in which to learn
+    learning_rate = .01 # **** this is the learning rate factor
+    number_of_iterations = 100
+
+    for i in range(number_of_iterations):
         for j in range(len(input_set)):
             mynn.feedforward(input_set[j])
             mynn.calc_error(labels[j])
-            mynn.back_prop(input_set[j])
-            #if (i == 1):
-            if (i % 10 == 0):
-                # print("Input batch shape= ", np.shape(mynn.batch))
-                # print("batch= ",mynn.batch)
-                # print("feed forward value= ",mynn.z1)
+            mynn.back_prop(input_set[j],learning_rate)
+
+            # track the error in the model
+            if (i % (number_of_iterations/10) == 0):
                 print("error= ",mynn.meanSquaredError)
-                # print("error shape= ",np.shape(mynn.error))
-                # print("Input batch transposed shape= ",np.shape(mynn.batch))
-                # print("transposed batch= ", mynn.batchT)
-                # print ("gradient weights shape = ",np.shape(mynn.gradientWeights))
-                # print("Gradient weights=",mynn.gradientWeights)
-                # print("activation= ",mynn.a1)
-                # print("weights1= ",mynn.weights1)
-                # print("Weights shape= ",np.shape(mynn.weights1))
-                # print("weightsAdj= ",mynn.weights1Adj)
 
+    # see what the final weights values are and bias value
+    print("\n final weights= ", mynn.weights1,"\n")
+    print("final bias= ", mynn.biases1,"\n")
 
-    # print("final weights= ", mynn.weights1)
-    # print("final bias= ", mynn.biases1)
     # print("final error= ", mynn.meanSquaredError)
-    print("\n")
     print("test [1, 0, 1] should be 0 = ", mynn.feedforward([[1, 0, 1]]))
     print("test [0, 1, 1] should be 1 = ", mynn.feedforward([[0, 1, 1]]))
+
+    # mynn.dotOperation("multiply_elements",input_set,labels)
