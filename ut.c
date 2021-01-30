82989fc3422e12d0b9c21ebdfa78b5b4e7e0a9e2
rising1 <phfrost@ymail.com>
2021-01-30 09:20:39 +0000

    Finiah

--- ut.c
+++ ut.c
@@ -22,25 +22,54 @@ int main(int argc, char* argv[]){
 
     set_up_model();
 
+
     double **weights1 =
         create_matrix(INPUTS_C,HIDDEN_SIZE);
+ 
+    prefill_matrix(weights1,INPUTS_C,HIDDEN_SIZE,0.5);
+    printM(weights1);
     double **bias1 =
         create_matrix(HIDDEN_SIZE,1);
-    double **hidden =
+    prefill_matrix(bias1,HIDDEN_SIZE,1,0.5);
+    /*double **hidden =
         create_matrix(HIDDEN_SIZE,1);
+    prefill_matrix(hidden,HIDDEN_SIZE,1,0.0);*/
     double **weights2 =
         create_matrix(HIDDEN_SIZE,OUTPUTS);
+    prefill_matrix(weights2,HIDDEN_SIZE,OUTPUTS,0.5);
     double **bias2 =
         create_matrix(OUTPUTS,1);
+    prefill_matrix(bias2,OUTPUTS,1,0.5);
     double **outputs =
         create_matrix(OUTPUTS,1);
+    prefill_matrix(outputs,OUTPUTS,1,0.0);
     double **weights1adj =
         create_matrix(INPUTS_C,HIDDEN_SIZE);
+    prefill_matrix(weights1adj,INPUTS_C,HIDDEN_SIZE,0.0);
     double **bias1adj =
         create_matrix(HIDDEN_SIZE,1);
+    prefill_matrix(bias1adj,HIDDEN_SIZE,1,0.0);    
     double **weights2adj =
         create_matrix(HIDDEN_SIZE,OUTPUTS);
+    prefill_matrix(weights2adj,HIDDEN_SIZE,OUTPUTS,0.0);
     double **bias2adj =
         create_matrix(OUTPUTS,1);
+    prefill_matrix(bias2adj,OUTPUTS,1,0.0);
 
+    double inputs[][3] = {{1.0, 1.0, 0.0}}; 
+    double targets[][1] = {{1.0},{0.0}};
+    double **inputsT =
+        create_matrix(INPUTS_C,INPUTS_R);   
+    
+    for(int i=0;i<1;i++)
+    {
+    	for(int j=0;j<3;j++)
+    	{
+    	inputsT[j][i] = inputs[i][j];
+    	printf("inputs %d%d\t %f\n",i,j,inputs[i][j]);
+    	}
+    }
+    //printM(weights1);
+    double **hidden = dot_mult(inputsT, weights1);
+   // printM(hidden);
 } 
--- ut.h
+++ ut.h
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 
@@ -7,7 +7,6 @@ double **create_matrix(int m, int n)
     double **p, *q;
     int i;
     assert(m>0 && n>0);
-
     p = (double **)malloc(m * sizeof(double *));
     if(p == NULL) return p;
     q = (double *)malloc(m * n * sizeof(double ));
@@ -16,8 +15,10 @@ double **create_matrix(int m, int n)
         return NULL;
     }
     for(i=0; i<m; ++i, q+=n)
+    {
         p[i] = q;
-
+    }
+    //printf("Psize= %d,%d\n",sizeof(p),sizeof(p[0]));
     return p;
 }
 
@@ -27,17 +28,59 @@ void destroy_matrix(double **p, int m, int n)
 {
     int i;
     assert(m>0 && n>0);
-
     for (i = 0; i < m; ++i)
-    free(p[i]);
+        free(p[i]);
     free(p);
 }
 
+/* The return type is actually `int (*)[r]`,
+but C doesn't like that. Call this function with
+int (*a)[2] = (int (*)[2])f_MatTrans(2, 3, x); */
+void printM(double **p) {
+	int r = sizeof(p[0])/sizeof(p[0][0]);
+	int c = sizeof(p[0][0]);
+	for(int i=0; i<r; i++) {
+        for(int j=0; j<c; j++) {
+            printf("matrix is r=%d c=%d [%d][%d] = %f\n",
+            	r,c,i,j,p[i][j]);
+        }
+    } 
+}
 
-
-void dot_mult(double **p1, double **p2)
+void prefill_matrix(double **p,int r,int c,double d)
 {
-	printf("in mult funtion");
+	for(int i=0;i<r;++i)
+	{
+		for(int j=0;j<c;++j)
+		{
+			p[i][j] = d;
+			//printf("p[%d][%d]=%f\n",i,j,p[i][j]);
+		}
+	}
+}
+
+
+double **dot_mult(double **p1, double **p2)
+{
+	
+	int r = sizeof(p1)/sizeof(p1[0]);
+	int c = sizeof(p1[0]);
+	int x = sizeof(p2)/sizeof(p2[0]);
+	int y = sizeof(p2[0]);
+	assert(c = x);
+	double **p = create_matrix(r, y);
+	for(int i=0;i<r;i++)
+	{
+		for(int j=0;j<r;j++)
+		{
+			for(int k=0;k<y;k++)
+			{
+				p[i][k] += p1[i][j] * p2[j][k];
+			}
+		}
+	}
+	
+	return p;
 }
 
 
